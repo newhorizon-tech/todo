bca4283bb81ecefcc5badb3d2e0acd613f220135
newhorizon-tech <alaukikinbox@gmail.com>
2021-09-23 01:17:48 +0530

    Fix package.json v2

--- src/index.js
+++ src/index.js
@@ -1,45 +1,43 @@
-import './style.css'
-
-console.log("Javascript works!")
+import './style.css';
 
 const tasks = [];
 
 const task1 = {
-  description: "Wash the dishes",
+  description: 'Wash the dishes',
   completed: false,
-  index: 3
-}
+  index: 3,
+};
 
 const task2 = {
-  description: "Clean my room",
+  description: 'Clean my room',
   completed: false,
-  index: 1
-}
+  index: 1,
+};
 
 const task3 = {
-  description: "Make breakfast",
+  description: 'Make breakfast',
   completed: false,
-  index: 2
-}
+  index: 2,
+};
 
 tasks.push(task1, task2, task3);
 
 const displayList = () => {
-  tasks.sort((a,b) => a.index - b.index);
-  const listElement = document.querySelector("#list");
+  tasks.sort((a, b) => a.index - b.index);
+  const listElement = document.querySelector('#list');
   listElement.innerHTML = '';
   tasks.forEach((task) => {
-    const taskElement = document.createElement("li");
+    const taskElement = document.createElement('li');
 
-    const check = document.createElement("input");
-    check.type = "checkbox";
+    const check = document.createElement('input');
+    check.type = 'checkbox';
 
-    const desc = document.createElement("span")
+    const desc = document.createElement('span');
     desc.textContent = task.description;
 
-    taskElement.append(check, desc)
+    taskElement.append(check, desc);
     listElement.append(taskElement);
-  })
-}
+  });
+};
 
 displayList();
--- webpack.config.js
+++ webpack.config.js
@@ -15,23 +15,23 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
-      template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   module: {
     rules: [{
-        test: /\.css$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+      test: /\.css$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        'style-loader',
+        // Translates CSS into CommonJS
+        'css-loader',
+      ],
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    },
 
     ],
   },
