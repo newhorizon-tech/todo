c93e416be0c1deedae35991b2bc53ab58df21a82
newhorizon-tech <alaukikinbox@gmail.com>
2021-09-22 21:54:16 +0530

    Add data structure for task list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './style.scss'
 
 console.log("Javascript works!")
+
+const tasks = [];
+
+const task1 = {
+  description: "Wash the dishes",
+  completed: false,
+  index: 1
+}
+
+const task2 = {
+  description: "Clean the room",
+  completed: false,
+  index: 2
+}
+
+tasks.push(task1,task2);
