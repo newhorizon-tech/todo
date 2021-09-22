9b07c40f9f0f439316f168a22d0d69f88fe87ce2
newhorizon-tech <alaukikinbox@gmail.com>
2021-09-23 00:25:04 +0530

    Remove SASS dependency

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.scss'
+import './style.css'
 
 console.log("Javascript works!")
 
--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,6 @@ module.exports = {
           'style-loader',
           // Translates CSS into CommonJS
           'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
         ],
       },
       {
